27683171a3fbe9fce8b5d79593667bbfe7a82671
cartheur <cartheur@pm.me>
2025-12-07 19:34:20 +0100

    Retarget

--- nativeforth.f
+++ nativeforth.f
@@ -1,7 +1,7 @@
 \ -*- text -*-
 
-\ The following words were defined natively on the original Richard W.M. Jones
-\ x86 forth. In this ARM port, they are defined in forth.
+\ The following words were defined natively on the original x86 forth.
+\ In this ARM port, they are defined in forth.
 
 : 0= 0 = ;
 : 0> 0 > ;
@@ -15,18 +15,8 @@
 : +! DUP @ ROT + SWAP ! ;
 
 \	A sometimes minimal FORTH compiler and tutorial for Linux / i386 systems. -*- asm -*-
-\	By Richard W.M. Jones <rich@annexia.org> http://annexia.org/forth
-\	This is PUBLIC DOMAIN (see public domain release statement below).
-\	$Id: jonesforth.f,v 1.18 2009-09-11 08:32:33 rich Exp $
 \
-\	The first part of this tutorial is in jonesforth.S.  Get if from http://annexia.org/forth
-\
-\	PUBLIC DOMAIN ----------------------------------------------------------------------
-\
-\	I, the copyright holder of this work, hereby release it into the public domain. This applies worldwide.
-\
-\	In case this is not legally possible, I grant any entity the right to use this work for any purpose,
-\	without any conditions, unless such conditions are required by law.
+\	The first part of this tutorial is in nativeforth.S.
 \
 \	SETTING UP ----------------------------------------------------------------------
 \
@@ -1655,7 +1645,7 @@
 
 : WELCOME
 	S" TEST-MODE" FIND NOT IF
-		." JONESFORTH VERSION " VERSION . CR
+		." HENRYFORTH VERSION " VERSION . CR
 		UNUSED . ." CELLS REMAINING" CR
 		." OK "
 	THEN
